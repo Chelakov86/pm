5d8a17839fc937216c0f42b147b0a9100c407ffe
Vladislav Chelakov <chelakov@gmail.com>
2026-05-06 21:25:48 +0200

    ui: implement fluid horizontal layout for board columns

--- frontend/src/components/KanbanBoard.tsx
+++ frontend/src/components/KanbanBoard.tsx
@@ -230,16 +230,17 @@ export const KanbanBoard = () => {
           onDragStart={handleDragStart}
           onDragEnd={handleDragEnd}
         >
-          <section className="grid gap-6 lg:grid-cols-5">
+          <section className="flex flex-row gap-6 h-[calc(100vh-280px)] min-h-[500px]">
             {board.columns.map((column) => (
-              <KanbanColumn
-                key={column.id}
-                column={column}
-                cards={column.cardIds.map((cardId) => board.cards[cardId])}
-                onRename={handleRenameColumn}
-                onAddCard={handleAddCard}
-                onDeleteCard={handleDeleteCard}
-              />
+              <div key={column.id} className="flex-1 min-w-[280px] max-w-[400px]">
+                <KanbanColumn
+                  column={column}
+                  cards={column.cardIds.map((cardId) => board.cards[cardId])}
+                  onRename={handleRenameColumn}
+                  onAddCard={handleAddCard}
+                  onDeleteCard={handleDeleteCard}
+                />
+              </div>
             ))}
           </section>
           <DragOverlay>
